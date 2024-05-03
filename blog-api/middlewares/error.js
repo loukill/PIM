f8ce6f0a7810022f1d2bb2431eef2141720a7bdc
Wassim brinssi <contactme.wassim@gmail.com>
2024-05-03 23:10:40 +0200

    new version

--- blog-api/middlewares/error.js
+++
@@ -1,3 +0,0 @@
-exports.handleAsyncError = (err, req, res, next) => {
-  res.status(500).json({ errro: err.message || err });
-};
--- blog-api/utils/helper.js
+++
@@ -1,6 +0,0 @@
-const FeaturedPost = require("../models/featuredPost");
-
-exports.isFeaturedPost = async (postId) => {
-  const post = await FeaturedPost.findOne({ post: postId });
-  return post ? true : false;
-};
